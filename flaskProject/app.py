94fd895d6f9e396a35c0ca2f37989b5af7994f38
riccardo30set <riccardo30set@gmail.com>
2023-05-18 08:38:45 +0200

    tolto login e register

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -14,10 +14,6 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'username' in session:
-        # Utente già loggato
-        username = session['username']
-        return f'Benvenuto, {username}! <a href="/logout">Logout</a>'
 
     if request.method == 'POST':
         search_query = request.form['search']
@@ -46,72 +42,6 @@ def index():
             return render_template('index.html', manga_data=None)
     else:
         return render_template('index.html', manga_data=None)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if 'username' in session:
-        # Utente già loggato
-        return redirect('/')
-
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        cur = mysql.connection.cursor()
-        # Esegui la query per verificare le credenziali dell'utente
-        query = "SELECT * FROM utente WHERE nickname = %s AND password = %s"
-        cur.execute(query, (username, password))
-        user = cur.fetchone()
-        print(user)
-        cur.close()
-
-        if user:
-            session['username'] = user[4]
-            return redirect('/')
-        else:
-            error_message = 'Username o password errati. Riprova.'
-            return render_template('login.html', error_message=error_message)
-    else:
-        return render_template('login.html')
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if 'username' in session:
-        # Utente già loggato
-        return redirect('/')
-
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        cur = mysql.connection.cursor()
-        # Check if the username is already taken
-        query = "SELECT * FROM utente WHERE nickname = %s"
-        cur.execute(query, (username,))
-        existing_user = cur.fetchone()
-
-        if existing_user:
-            error_message = 'Username already exists. Please choose a different username.'
-            return render_template('register.html', error_message=error_message)
-        else:
-            # Insert the new user into the database
-            query = "INSERT INTO utente (nickname, password) VALUES (%s, %s)"
-            cur.execute(query, (username, password))
-            mysql.connection.commit()
-            cur.close()
-
-            session['username'] = username
-            return redirect('/')
-    else:
-        return render_template('register.html')
-
-
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect('/')
 
 
 def get_manga_by_title(title):
