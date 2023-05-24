db527d86a854059ea3f1b06479722341f84d3ef0
EmanuelFadda <emanuel.fadda@hotmail.com>
2023-05-24 17:20:26 +0200

    aggiunte modifiche ad app.py e profile.html

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -11,93 +11,129 @@ app.config['MYSQL_DB'] = 'mangarealm'
 
 mysql = MySQL(app)
 
-MANGA_FOR_PAGE=18
+MANGA_FOR_PAGE = 18
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        result=None
-        if len(request.form)==1:
+        result = None
+        if len(request.form) == 1:
             titolo = request.form['search']
-            ids=get_ids_for_page(title=titolo)
-            result=get_manga_by_list_id(tuple(ids))
-            
+            ids = get_ids_for_page(title=titolo)
+            result = get_manga_by_list_id(tuple(ids))
+
         else:
-            genre=request.form['genre']
-            author=request.form['author']
-            artist=request.form['artist']
-            year=request.form['year']
-            ids=get_ids_for_page(genre=genre,author=author,artist=artist,year=year)
-            result=get_manga_by_list_id(tuple(ids))
+            genre = request.form['genre']
+            author = request.form['author']
+            artist = request.form['artist']
+            year = request.form['year']
+            ids = get_ids_for_page(
+                genre=genre, author=author, artist=artist, year=year)
+            result = get_manga_by_list_id(tuple(ids))
         if result:
-            return render_template('index.html', manga_data=result, genres=get_all_genres(),people=get_all_person())
+            return render_template('index.html', manga_data=result, genres=get_all_genres(), people=get_all_person())
         else:
-            return render_template('index.html', manga_data=None, genres=get_all_genres(),people=get_all_person())
+            return render_template('index.html', manga_data=None, genres=get_all_genres(), people=get_all_person())
     else:
-        return render_template('index.html', manga_data=None, genres=get_all_genres(),people=get_all_person())
+        return render_template('index.html', manga_data=None, genres=get_all_genres(), people=get_all_person())
 
 
 @app.route('/manga/<int:id>')
 def manga_details(id):
     manga = get_manga_by_list_id((id,))[0]
-    id_manga=manga[0]
+    id_manga = manga[0]
 
-    #recupero capitoli del manga
-    chapters=get_chapters_by_manga_id(id)
-    
-    #recupero commenti del manga
-    comments=get_comments_by_manga_id(id)
+    # recupero capitoli del manga
+    chapters = get_chapters_by_manga_id(id)
+
+    # recupero commenti del manga
+    comments = get_comments_by_manga_id(id)
 
     if manga:
-        return render_template('manga.html',genres=get_all_genres(),people=get_all_person(), manga=manga,chapters=chapters,comments=comments)
+        return render_template('manga.html', genres=get_all_genres(), people=get_all_person(), manga=manga, chapters=chapters, comments=comments)
     else:
-        return render_template('error.html',genres=get_all_genres(),people=get_all_person(), message='Manga not found')
+        return render_template('error.html', genres=get_all_genres(), people=get_all_person(), message='Manga not found')
 
 
-@app.route('/account/<nome_account>')
-def profile(nome_account):
-    x = nome_account
-    return render_template('profile.html', genres=get_all_genres(),people=get_all_person(),nome_account=x)
+@app.route('/account/<nickname_account>')
+def profile(nickname_account):
+    # aggiungere controllo se non esiste l'account
+    id_manga = 12
+    titolo = "titolo"
+    copertina = "url_copertina"
+    descrizione = "descrizione"
+    anno = 1900
+
+    manga_object = {
+        "ID": id_manga,
+        "titolo": titolo,
+        "copertina": copertina,
+        "descrizione": descrizione,
+        "anno": anno
+    }
+
+    # funzione per controllare l'esistenza dell'account
+    # funzione per trovare tutti i manga visti
+    # funzione per trovare tutti i manga piaciuti
+    nickname = nickname_account
+    nome = "nome"
+    cognome = "cognome"
+    email = "email"
+    id = 12
+    manga_visti = [manga_object, manga_object, manga_object]
+    manga_piaciuti = [manga_object, manga_object, manga_object, manga_object]
+
+    utente = {
+        "id": id,
+        "nickname": nickname,
+        "nome": nome,
+        "cognome": cognome,
+        "email": email,
+        "manga_visti": manga_visti,
+        "manga_piaciuti": manga_piaciuti,
+    }
+
+    return render_template('profile.html', utente=utente)
 
 
 @app.route('/manga/<int:idm>/capitolo/<int:nc>')
-def capitolo(idm,nc):
+def capitolo(idm, nc):
 
-    #richiesta capitoli dal database
-    
-    chapters=get_chapters_by_manga_id(idm)
+    # richiesta capitoli dal database
 
-    #ricerca del capitolo
-    chap=chapters[0]
+    chapters = get_chapters_by_manga_id(idm)
+
+    # ricerca del capitolo
+    chap = chapters[0]
     for c in chapters:
-        
-        if c[1]==nc:
-            chap=c
+
+        if c[1] == nc:
+            chap = c
             break
-        
 
-    #url delle immagini
-    url=chap[3]
-    extension=url[-3:]
-    start_url=url[:-5]
-    pages=1
+    # url delle immagini
+    url = chap[3]
+    extension = url[-3:]
+    start_url = url[:-5]
+    pages = 1
 
-    #calcolo numero di immagini
-    imgs=[]     
-    #controllo tipo dell'immagini
+    # calcolo numero di immagini
+    imgs = []
+    # controllo tipo dell'immagini
     while exists(start_url+str(pages)+"."+extension):
         imgs.append(start_url+str(pages)+"."+extension)
-        pages+=1
+        pages += 1
 
-    if extension=="jpg":
-        extension="png"
+    if extension == "jpg":
+        extension = "png"
     else:
-        extension="jpg"
+        extension = "jpg"
 
     while exists(start_url+str(pages)+"."+extension):
         imgs.append(start_url+str(pages)+"."+extension)
-        pages+=1
-    return render_template('capitolo.html',genres=get_all_genres(),people=get_all_person(), imgs=imgs, chapters=chapters, chp=nc)
+        pages += 1
+    return render_template('capitolo.html', genres=get_all_genres(), people=get_all_person(), imgs=imgs, chapters=chapters, chp=nc)
 
 
 def exists(path):
@@ -140,7 +176,7 @@ def get_chapters_by_manga_id(id):
             ORDER BY numeroVolume,numeroCapitolo ASC
             """
     cur.execute(query, (id,))
-    chapters=cur.fetchall()
+    chapters = cur.fetchall()
     return chapters
 
 
@@ -153,51 +189,51 @@ def get_comments_by_manga_id(id):
             WHERE ID_Manga=%s
             """
     cur.execute(query, (id,))
-    chapters=cur.fetchall()
+    chapters = cur.fetchall()
     return chapters
 
 
-def get_ids_for_page(title="",genre="",author="",artist="",year="",page=1):
+def get_ids_for_page(title="", genre="", author="", artist="", year="", page=1):
     cur = mysql.connection.cursor()
-    first=True
-    base_query="""SELECT manga.id
+    first = True
+    base_query = """SELECT manga.id
         FROM manga
     """
-    join=""
-    where="WHERE 1=1"
-    params=[]
-    limit=" LIMIT "+str(MANGA_FOR_PAGE)
-    skip=" SKIP "+str(MANGA_FOR_PAGE*(page-1))
-    #filtra per nome
-    if title!= "":
-        where+=" AND titolo LIKE %s"
+    join = ""
+    where = "WHERE 1=1"
+    params = []
+    limit = " LIMIT "+str(MANGA_FOR_PAGE)
+    skip = " SKIP "+str(MANGA_FOR_PAGE*(page-1))
+    # filtra per nome
+    if title != "":
+        where += " AND titolo LIKE %s"
         params.append('%'+title+'%')
 
-    #filtra per genere
-    if genre!= "":
-        join+= "JOIN tipomanga ON manga.id=tipomanga.ID_Manga "
-        where+=" AND ID_categoria=%s"
+    # filtra per genere
+    if genre != "":
+        join += "JOIN tipomanga ON manga.id=tipomanga.ID_Manga "
+        where += " AND ID_categoria=%s"
         params.append(int(genre))
 
-    #filtra per autore
-    if author!= "":
-        join+=" JOIN scrive ON manga.id=scrive.ID_Manga "
-        where+=" AND ID_Autore=%s"
+    # filtra per autore
+    if author != "":
+        join += " JOIN scrive ON manga.id=scrive.ID_Manga "
+        where += " AND ID_Autore=%s"
         params.append(int(author))
-        
-    #filtra per artista
-    if artist!= "":
-        join+=" JOIN disegna ON manga.id=disegna.ID_Manga "
-        where+=" AND ID_Artista=%s"
+
+    # filtra per artista
+    if artist != "":
+        join += " JOIN disegna ON manga.id=disegna.ID_Manga "
+        where += " AND ID_Artista=%s"
         params.append(int(artist))
 
-    #filtra per anno
+    # filtra per anno
     if year != "":
-        where+=" AND Anno=%s"
+        where += " AND Anno=%s"
         params.append(int(year))
     print(base_query+join+where+limit)
     cur.execute(base_query+join+where+limit, params)
-    ids=[]
+    ids = []
     for manga in cur.fetchall():
         ids.append(manga[0])
     return tuple(ids)
@@ -207,7 +243,7 @@ def get_manga_by_list_id(ids):
     try:
         cur = mysql.connection.cursor()
 
-        #palcehoder per evitare l'sql injection
+        # palcehoder per evitare l'sql injection
         placeholders = "("+(','.join(['%s'] * len(ids)))+")"
 
         query = f"""
